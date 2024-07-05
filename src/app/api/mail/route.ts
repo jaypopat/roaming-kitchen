d62c68889fc1f8df7eb3fdf1b69579a5742957cb
Jay Popat <contact.jaypopat@gmail.com>
2024-07-05 02:13:24 +0100

    fixed build issues

--- src/app/api/mail/route.ts
+++ src/app/api/mail/route.ts
@@ -1,39 +1,44 @@
-import { NextRequest } from 'next/server';
-import nodemailer from 'nodemailer';
+  import { NextRequest } from 'next/server';
+  import nodemailer from 'nodemailer';
 
-export default async function POST(req:NextRequest) {
-
-    console.log("endpoint got hit");
-    const formData = await req.json(); 
-
-    const { name, email, message } = formData;
-
-    const transporter = nodemailer.createTransport({
-      service: 'Gmail',
-      auth: {
-        user: "contact.jaypopat@gmail.com",
-        pass: "x",
-      },  
-    });
-
-    // Define the email options
-    const mailOptions = {
-      from: "contact.jaypopat@gmail.com",
-      to: "contact.jaypopat@gmail.com",
-      subject: `New Contact Form Submission from ${name}`,
-      text: `
-        Name: ${name}
-        Email: ${email}
-        Message: ${message}
-      `,
-    };
+  export async function POST(request: NextRequest){
 
     try {
-      // Send the email
-      await transporter.sendMail(mailOptions);
-      return new Response('Email sent successfully', { status: 200 });
-    } catch (error) {
-      console.error('Error sending email:', error);
-      return new Response('Error sending email', { status: 500 });
-    }
+
+      console.log("endpoint got hit");
+      const formData = await request.json(); 
+
+      const { name, email, message } = formData;
+
+      const transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+          user: "contact.jaypopat@gmail.com",
+          pass: "x",
+        },  
+      });
+
+      // Define the email options
+      const mailOptions = {
+        from: "contact.jaypopat@gmail.com",
+        to: "contact.jaypopat@gmail.com",
+        subject: `New Contact Form Submission from ${name}`,
+        text: `
+          Name: ${name}
+          Email: ${email}
+          Message: ${message}
+        `,
+      };
+
+      try {
+        // Send the email
+        await transporter.sendMail(mailOptions);
+        return new Response('Email sent successfully', { status: 200 });
+      } catch (error) {
+        console.error('Error sending email:', error);
+        return new Response('Error sending email', { status: 500 });
+      }
+    } catch (error: any) {
+      return new Response('Error parsing form data', { status: 400 });
   }
+    }
