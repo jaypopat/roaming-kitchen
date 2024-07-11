9ca192446d67c232b770197b7bdf69d3817e29e8
Jay Popat <contact.jaypopat@gmail.com>
2024-07-12 00:34:04 +0100

    changed layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,22 @@
-import { Inter } from 'next/font/google';
-import './globals.css';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <>
-    <Header/>
       <body className="flex flex-col min-h-screen">
-          <main className='flex-grow'>{children}</main>
+        <Header />
+        <main className="flex-grow">{children}</main>
+        <Footer />
       </body>
-    <Footer/> 
     </>
   );
 }
