1590f4661948f457b1ca1e3f9426dd0088157e22
Jay Popat <contact.jaypopat@gmail.com>
2024-07-12 12:08:26 +0100

    added socials

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Socials from "@/components/socials";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow">{children}</main>
+        <Socials />
         <Footer />
       </body>
     </html>
