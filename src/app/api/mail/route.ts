5e46716a67896daa22a6d624d955a8cd437363f4
Jay Popat <contact.jaypopat@gmail.com>
2024-07-12 04:15:07 +0100

    added .env support for credentials

--- src/app/api/mail/route.ts
+++ src/app/api/mail/route.ts
@@ -11,7 +11,7 @@ export async function POST(request: NextRequest) {
     const transporter = nodemailer.createTransport({
       service: "Gmail",
       auth: {
-        user: "contact.jaypopat@gmail.com",
+        user: process.env.GMAIL_USER,
         pass: process.env.GMAIL_PWD,
       },
     });
