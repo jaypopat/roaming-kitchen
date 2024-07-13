eb32acec3fd14bd3458b26944970d828949d158b
Jay Popat <contact.jaypopat@gmail.com>
2024-07-13 01:18:27 +0100

    making the links in the navbar modular and making the menu filtering logic completely modular based on the json file

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,44 @@
-import Image from "next/image"
-import Link from "next/link"
-export default function Header(){
-    return(
-        <header className="fixed top-0 left-0 right-0 px-4 lg:px-6 h-14 flex items-center bg-primary text-primary-foreground z-50">
-        <Link href="/" className="flex items-center justify-center" prefetch={false}>
-          <Image height={80} width={80} src="/logo.png" alt="" />
-        </Link>
-        <nav className="ml-auto flex gap-4 sm:gap-6">
-          <Link href="/menu" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
-            Menu
+import Image from "next/image";
+import Link from "next/link";
+export default function Header() {
+  const links = [
+    {
+      name: "Menu",
+      href: "/menu",
+    },
+    {
+      name: "About",
+      href: "/about",
+    },
+    {
+      name: "Chef",
+      href: "/chef",
+    },
+    {
+      name: "Contact",
+      href: "/contact",
+    },
+  ];
+  return (
+    <header className="fixed top-0 left-0 right-0 px-4 lg:px-6 h-14 flex items-center bg-primary text-primary-foreground z-50">
+      <Link
+        href="/"
+        className="flex items-center justify-center"
+        prefetch={false}
+      >
+        <Image height={80} width={80} src="/logo.png" alt="" />
+      </Link>
+      <nav className="ml-auto flex gap-4 sm:gap-6">
+        {links.map((link, index) => (
+          <Link
+            key={index}
+            href={link.href}
+            className="text-lg sm:text-xl font-semibold hover:text-accent"
+          >
+            {link.name}
           </Link>
-          <Link href="/about" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
-            About
-          </Link>
-          <Link href="/chef" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
-            Chef
-          </Link>
-          <Link href="/contact" className="text-sm font-medium hover:underline underline-offset-4" prefetch={false}>
-            Contact
-          </Link>
-        </nav>
-      </header>
-    )
+        ))}
+      </nav>
+    </header>
+  );
 }
