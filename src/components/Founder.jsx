8aecfb40113197db1b77b3c1505e177593d2500d
Jay Popat <contact.jaypopat@gmail.com>
2024-08-06 15:01:35 +0100

    changed founder image sizes

--- src/components/Founder.jsx
+++ src/components/Founder.jsx
@@ -3,15 +3,18 @@ import { Card } from "./ui/card";
 export default function Founder({ role, photo, name }) {
   return (
     <div className="flex flex-col items-center justify-center">
-      <Card>
-        <Image
-          alt={name}
-          src={photo}
-          width={300}
-          height={600}
-          className="aspect-video object-cover rounded-md"
-        />
-      </Card>
+      <div className="w-[300px] h-[400px] overflow-hidden rounded-md">
+        <div className="relative w-full h-full">
+          <Image
+            alt={name}
+            src={photo}
+            width={300}
+            height={400}
+            objectFit="cover"
+            sizes="300px"
+          />
+        </div>
+      </div>
       <h3 className="text-xl font-bold">{name}</h3>
       <p className="text-muted-foreground">{role}</p>
     </div>
