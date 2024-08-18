2f1970396f76c000a7531904bb306659051b3a87
Jay Popat <contact.jaypopat@gmail.com>
2024-08-18 20:39:58 +0100

    added google analytics and fixed build issues

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Socials from "@/components/socials";
-import { Analytics } from "@vercel/analytics/react";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,7 +22,7 @@ export default function RootLayout({
       <body className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow">{children}</main>
-        <Analytics />
+        <GoogleAnalytics gaId="GTM-PJDHW4K7" />
         <Socials />
         <Footer />
       </body>
--- src/components/MenuLayout.tsx
+++ src/components/MenuLayout.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { MenuItems } from "@/components/MenuItem";
 import DietaryFilter from "@/components/DietaryFilter";
