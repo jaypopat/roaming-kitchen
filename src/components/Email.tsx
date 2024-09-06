282b7e720107179dbe68f8c964003fb4ea937d4f
Jay Popat <contact.jaypopat@gmail.com>
2024-09-06 04:14:27 +0100

    added styles to the email

--- src/components/Email.tsx
+++ src/components/Email.tsx
@@ -1,15 +1,97 @@
-import { Html, Button } from "@react-email/components";
-import { FC } from "react";
+import {
+  Body,
+  Button,
+  Container,
+  Head,
+  Hr,
+  Html,
+  Img,
+  Preview,
+  Section,
+  Text,
+} from "@react-email/components";
+import * as React from "react";
 
 interface EmailProps {
   name: string;
   description: string;
 }
-export const EmailComponent: FC<EmailProps> = ({ name, description }) => {
-  return (
-    <Html lang="en">
-      <p>Hello {name}!</p>
-      <p>{description}</p>
-    </Html>
-  );
+
+export const Email = ({ name, description }: EmailProps) => (
+  <Html>
+    <Head />
+    <Preview>The food truck that comes to you</Preview>
+    <Body style={main}>
+      <Container style={container}>
+        <Img
+          src="/logo.png"
+          width="170"
+          height="50"
+          alt="Roaming Kitchen"
+          style={logo}
+        />
+        <Text style={paragraph}>Hi {name},</Text>
+        <Text style={paragraph}>{description}</Text>
+        <Section style={btnContainer}>
+          <Button style={button} href="https://roaming.kitchen">
+            Learn more about us
+          </Button>
+        </Section>
+        <Text style={paragraph}>
+          Best,
+          <br />
+          The Roaming Kitchen Team
+        </Text>
+        <Hr style={hr} />
+        <Text style={footer}>
+          We are on wheels.. Tell us where to park or deliver
+        </Text>
+      </Container>
+    </Body>
+  </Html>
+);
+
+const main = {
+  backgroundColor: "#ffffff",
+  fontFamily:
+    '-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif',
+};
+
+const container = {
+  margin: "0 auto",
+  padding: "20px 0 48px",
+};
+
+const logo = {
+  margin: "0 auto",
+};
+
+const paragraph = {
+  fontSize: "16px",
+  lineHeight: "26px",
+};
+
+const btnContainer = {
+  textAlign: "center" as const,
+};
+
+const button = {
+  backgroundColor: "#5F51E8",
+  borderRadius: "3px",
+  color: "#fff",
+  fontSize: "16px",
+  textDecoration: "none",
+  textAlign: "center" as const,
+  display: "block",
+  padding: "12px",
+};
+
+const hr = {
+  borderColor: "#cccccc",
+  margin: "20px 0",
+};
+
+const footer = {
+  color: "#8898aa",
+  fontSize: "12px",
 };
