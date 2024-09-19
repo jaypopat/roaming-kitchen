8698a522c62b43e46999bb5b47c22d2da3824e1f
Vipul Popat <vipulpopat@gmail.com>
2024-09-19 15:20:01 +0100

    removal of anouncement text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,13 +32,6 @@ export default function Page() {
               alt="Roaming Kitchen Logo"
               className="mx-auto animate-pulse"
             />
-            <h1 className="text-4xl font-bold tracking-wide">
-              Something Delicious is Coming!
-            </h1>
-            <p className="max-w-2xl mx-auto text-lg md:text-2xl text-white/90">
-              We're busy cooking up something special for you. Stay tuned to our
-              socials and be the first to know!
-            </p>
             <Link
               href="/menu"
               className="mt-6 inline-flex h-12 items-center justify-center rounded-md bg-red-600 px-8 text-lg font-semibold text-white shadow-lg hover:bg-red-700 transition-all duration-300"
